51a3ab59d695626bfd566ee73193cd1bbfa12bc9
Daniel Kelly <me@danielkelly.io>
2022-03-08 15:47:34 -0600

    7. typing refs

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import type Emoji from "@/types/Emoji";
+import { ref } from "vue";
+const text = ref("");
+const emoji = ref<Emoji | null>(null);
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
-    <textarea placeholder="New Journal Entry for danielkelly_io"></textarea>
-    <EmojiField />
+    <textarea
+      v-model="text"
+      placeholder="New Journal Entry for danielkelly_io"
+    ></textarea>
+    <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
       <span>0 / 280</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
---
+++ src/types/Emoji.ts
@@ -0,0 +1,17 @@
+type Emoji =
+  | "confused"
+  | "cool"
+  | "crying"
+  | "grinning-squinting"
+  | "happy"
+  | "heart-eyes"
+  | "laugh"
+  | "sad"
+  | "shocked"
+  | "smiling"
+  | "smiling-with-eyes"
+  | "surprised"
+  | "wink"
+  | "worried";
+
+export default Emoji;
